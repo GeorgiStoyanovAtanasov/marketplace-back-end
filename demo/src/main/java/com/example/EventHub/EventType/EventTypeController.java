e2845f2328dfb63a21e344c4b16a3d38f311a05e
MirkaaStefanov <mirkanstefanov2007@gmail.com>
2024-07-18 13:11:04 +0300

    Add update and delete

--- demo/src/main/java/com/example/EventHub/EventType/EventTypeController.java
+++ demo/src/main/java/com/example/EventHub/EventType/EventTypeController.java
@@ -8,18 +8,28 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/event-type")
 public class EventTypeController {
+
+    private EventTypeRepository eventTypeRepository;
+    private EventTypeMapper eventTypeMapper;
+
     @Autowired
-    EventTypeRepository eventTypeRepository;
+    public EventTypeController(EventTypeRepository eventTypeRepository, EventTypeMapper eventTypeMapper){
+        this.eventTypeRepository = eventTypeRepository;
+        this.eventTypeMapper = eventTypeMapper;
+    }
 
     @PostMapping("/add")
-    public void postProduct(@Valid @ModelAttribute EventType eventType, BindingResult bindingResult) {
+    public void postProduct(@Valid @ModelAttribute EventTypeDTO eventTypeDTO, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
             throw new IllegalArgumentException();
         } else {
+            EventType eventType = eventTypeMapper.toEntity(eventTypeDTO);
             eventTypeRepository.save(eventType);
         }
     }
@@ -28,4 +38,22 @@ public class EventTypeController {
         Iterable<EventType> allEventTypes = eventTypeRepository.findAll();
         return allEventTypes;
     }
+    @DeleteMapping("/delete")
+    public void deleteEventType(@RequestParam("id") Integer id){
+        Optional<EventType> foundEventType = eventTypeRepository.findById(id);
+        if(foundEventType.isPresent()){
+            eventTypeRepository.deleteById(id);
+        }
+    }
+
+    @PutMapping("/update")
+    public void updateEventType(@RequestBody EventTypeDTO eventTypeDTO, BindingResult bindingResult){
+        if (bindingResult.hasErrors()) {
+            throw new IllegalArgumentException();
+        } else {
+            EventType eventType = eventTypeMapper.toEntity(eventTypeDTO);
+            eventTypeRepository.save(eventType);
+        }
+    }
+
 }
