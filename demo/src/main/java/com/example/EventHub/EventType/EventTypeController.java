f841982e6f557f370caa6523679f2b8b9b4d5e05
MirkaaStefanov <mirkanstefanov2007@gmail.com>
2024-07-18 12:43:02 +0300

    Make it rest

--- demo/src/main/java/com/example/EventHub/EventType/EventTypeController.java
+++ demo/src/main/java/com/example/EventHub/EventType/EventTypeController.java
@@ -5,14 +5,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/event-type")
 public class EventTypeController {
     @Autowired
