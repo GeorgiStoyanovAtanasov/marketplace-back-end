e4295dbfa582db3da7d0ae8807b6249b17775444
MirkaaStefanov <mirkanstefanov2007@gmail.com>
2024-07-20 14:24:22 +0300

    fix event dto

--- demo/src/main/java/com/example/EventHub/Organisation/OrganisationController.java
+++ demo/src/main/java/com/example/EventHub/Organisation/OrganisationController.java
@@ -13,8 +13,9 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+
 @RequestMapping("/organisation")
+@RestController
 public class OrganisationController {
     @Autowired
     OrganisationRepository organisationRepository;
