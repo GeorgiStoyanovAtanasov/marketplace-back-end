204c9440d171883913d5dc80260ab51cc78901c9
alisayali <alisayerecebali@gmail.com>
2024-07-31 15:29:33 +0300

    organisation update and delete

--- demo/src/main/java/com/example/EventHub/Organisation/OrganisationService.java
+++ demo/src/main/java/com/example/EventHub/Organisation/OrganisationService.java
@@ -3,46 +3,35 @@ package com.example.EventHub.Organisation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 
 import java.util.Optional;
 
 @Service
 public class OrganisationService {
-    @Autowired
-    OrganisationRepository organisationRepository;
-    public String updateForm(Integer id, Model model) {
 
+    OrganisationRepository organisationRepository;
+
+    OrganisationMapper organisationMapper;
+
+    @Autowired
+    public OrganisationService(OrganisationRepository organisationRepository, OrganisationMapper organisationMapper) {
+        this.organisationRepository = organisationRepository;
+        this.organisationMapper=organisationMapper;
+    }
+
+    public void postUpdate(Integer id, OrganisationDTO updatedOrganisation) {
+        Organisation organisation = organisationMapper.toEntity(updatedOrganisation);
         Optional<Organisation> optionalOrganisation = organisationRepository.findById(id);
         if (optionalOrganisation.isPresent()) {
-            Organisation organisation = organisationRepository.findById(id).get();
-            model.addAttribute("updateOrganisation", organisation);
-            return "organisation-update-form";
-        } else {
-            return "id could not be found";
-        }
-    }
-
-    public String postUpdate(Integer id, Organisation updatedOrganisation, BindingResult bindingResult, Model model) {
-
-        if (bindingResult.hasErrors()) {
-            return "organisation-update-form";
-        } else {
-            Organisation organisation = organisationRepository.findById(id).get();
-            getOrganisation(organisation, updatedOrganisation);
+            organisation.setId(id);
             organisationRepository.save(organisation);
-            model.addAttribute("organisation", organisation);
-            return "organisation-update-result";
+        }else {
+            throw new IllegalArgumentException();
         }
-    }
-
-    private Organisation getOrganisation(Organisation organisation, Organisation updatedOrganisation) {
-        organisation.setName(updatedOrganisation.getName());
-        return organisation;
     }
 
     public String delete(Integer id, Model model) {
-        Organisation organisation= organisationRepository.findById(id).get();
+        Organisation organisation = organisationRepository.findById(id).get();
         organisationRepository.delete(organisation);
         model.addAttribute("organisation", organisation);
         return "organisation-delete";
