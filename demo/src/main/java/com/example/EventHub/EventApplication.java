a7a2227f0c7991153487a0088e75b523e9f2a43b
first-branch <radeva7609@gmaill.com>
2024-07-31 19:55:45 +0300

    add logging

--- demo/src/main/java/com/example/EventHub/EventApplication.java
+++ demo/src/main/java/com/example/EventHub/EventApplication.java
@@ -5,12 +5,17 @@ import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @SpringBootApplication
 @EnableFeignClients
 @OpenAPIDefinition(info = @Info(title = "JWT", description = "jwt"))
 public class EventApplication {
+    private static final Logger logger = LogManager.getLogger(EventApplication.class);
     public static void main(String[] args) {
+        logger.warn("4.This is a WARN message.");
+        logger.error("5.This is an ERROR message.");
 
         SpringApplication.run(EventApplication.class, args);
     }
