4e6a34a62f2836c3d65a3592cbb96f2156f684dc
first-branch <radeva7609@gmaill.com>
2024-08-12 11:35:02 +0300

    make admin be able to accept organisations

--- src/test/java/com/example/EventHub/OrganisationControllerTest.java
+++ src/test/java/com/example/EventHub/OrganisationControllerTest.java
@@ -1,49 +1,49 @@
-package com.example.EventHub;
-
-import com.example.EventHub.Manager.Manager;
-import com.example.EventHub.Manager.ManagerRepository;
-import com.example.EventHub.Organisation.*;
-import com.example.EventHub.Role.Role;
-import com.example.EventHub.User.User;
-import com.example.EventHub.User.UserRepository;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.annotation.Rollback;
-import org.springframework.test.context.ActiveProfiles;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-@SpringBootTest
-@ActiveProfiles({"testdb"})
-@ExtendWith(DbSetupExtension.class)
-public class OrganisationControllerTest {
-    private OrganisationController organisationController;
-
-    private UserRepository userRepository;
-
-    private ManagerRepository managerRepository;
-
-    private OrganisationRepository organisationRepository;
-
-    @Autowired
-    public OrganisationControllerTest(OrganisationController organisationController, UserRepository userRepository, ManagerRepository managerRepository, OrganisationRepository organisationRepository, OrganisationMapper organisationMapper) {
-        this.organisationController = organisationController;
-        this.userRepository = userRepository;
-        this.managerRepository = managerRepository;
-        this.organisationRepository = organisationRepository;
-    }
-
-    private OrganisationDTO organisationDTO;
-
-    @BeforeEach
-    void setUp() {
-        organisationDTO = new OrganisationDTO(1, "Test Org");
-    }
-
+//package com.example.EventHub;
+//
+//import com.example.EventHub.Manager.Manager;
+//import com.example.EventHub.Manager.ManagerRepository;
+//import com.example.EventHub.Organisation.*;
+//import com.example.EventHub.Role.Role;
+//import com.example.EventHub.User.User;
+//import com.example.EventHub.User.UserRepository;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.extension.ExtendWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.security.test.context.support.WithMockUser;
+//import org.springframework.test.annotation.Rollback;
+//import org.springframework.test.context.ActiveProfiles;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//@SpringBootTest
+//@ActiveProfiles({"testdb"})
+//@ExtendWith(DbSetupExtension.class)
+//public class OrganisationControllerTest {
+//    private OrganisationController organisationController;
+//
+//    private UserRepository userRepository;
+//
+//    private ManagerRepository managerRepository;
+//
+//    private OrganisationRepository organisationRepository;
+//
+//    @Autowired
+//    public OrganisationControllerTest(OrganisationController organisationController, UserRepository userRepository, ManagerRepository managerRepository, OrganisationRepository organisationRepository, OrganisationMapper organisationMapper) {
+//        this.organisationController = organisationController;
+//        this.userRepository = userRepository;
+//        this.managerRepository = managerRepository;
+//        this.organisationRepository = organisationRepository;
+//    }
+//
+//    private OrganisationDTO organisationDTO;
+//
+//    @BeforeEach
+//    void setUp() {
+//        organisationDTO = new OrganisationDTO(1, "Test Org", OrganisationPermission.ACCEPT);
+//    }
+//
 //    @Test
 //    @WithMockUser(username = "test@example.com")
 //    @Rollback
@@ -58,7 +58,7 @@ public class OrganisationControllerTest {
 //        manager.setUser(user);
 //        managerRepository.save(manager);
 //
-//        organisationController.addOrganisation(organisationDTO, manager.getId());
+//        organisationController.addOrganisation(organisationDTO);
 //
 //        Organisation savedOrganisation = organisationRepository.findByName("Test Org");
 //        assertNotNull(savedOrganisation, "Organisation should be saved in the database");
@@ -66,14 +66,14 @@ public class OrganisationControllerTest {
 //        Manager updatedManager = managerRepository.findByUser(user);
 //        assertEquals(savedOrganisation, updatedManager.getOrganisation(), "Manager should be associated with the new organisation");
 //    }
-
-//    @Test
-//    @WithMockUser(username = "nonexistent@example.com")
-//    void testAddOrganisation_ManagerNotFound() {
-//        boolean actual = organisationController.addOrganisation(organisationDTO, 0);
-//        assertEquals(false, actual);
-//    }
-
-
-}
-
+//
+////    @Test
+////    @WithMockUser(username = "nonexistent@example.com")
+////    void testAddOrganisation_ManagerNotFound() {
+////        boolean actual = organisationController.addOrganisation(organisationDTO);
+////        assertEquals(false, actual);
+////    }
+//
+//
+//}
+//
